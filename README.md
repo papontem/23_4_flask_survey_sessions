a460e97be275ff2c22a03daabf5493aebfe712b8
papontemjetix <papontem@gmail.com>
2023-06-26 19:17:42 -0400

    exercise complete, step 8 done

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # 23_4_flask_survey_sessions
-public repo of unit 23.4 exercise continuing the flask survey but with the suse of flask sessions
+
+public repo of unit 23.4 exercise continuing the flask survey but with the use of flask sessions
--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # save this as app.py
 from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
-from surveys import *
+from surveys import surveys
 
 #flask requiered app name
 app = Flask(__name__)
@@ -11,21 +11,38 @@ app.config['SECRET_KEY'] = "oh-so-secret"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app) 
 
-# PAM: outmost storage of users RESPONSES as they travers through rpute ioon their way to complete survey
-RESPONSES = []
 # PAM: You have the avility to chose between two surveys. surveys = {"satisfaction": satisfaction_survey,"personality": personality_quiz}
 selected_survey = surveys["satisfaction"] #focusing on this one for now
 # selected_survey = surveys["personality"]
 
 @app.route('/') #decorator expecting a function
 def homepage(): #the function that will be executed when decorator is flagged
-    """TODO: Step 2
-    Show homepage. 
-    PAM: render template for startpage that shows the user the title of the survey, the instructions, and a button to start the survey.
+    """TODO: Step 2 Show homepage. 
+    render template for startpage that shows the user the title of the survey, the instructions, and a button to start the survey.
+
+    PAM: DONE
+
+    TODO: Step 8: Storing answers in a list on the server.
+    Use the session to store response information.
+
+    To begin, modify your start page so that clicking on the button fires off a POST request to a new route that will set session[“responses”] to an empty list. The view function should then redirect you to the start of the survey. (This will also take care of the issue mentioned at the end of Step Six.) Then, modify your code so that you reference the session when you're trying to edit the list of responses.
+
     """
     
-
     return render_template("startpage.html", selected_survey=selected_survey)
+
+@app.route('/start', methods=["POST"])
+def start_survey():
+    """
+    route from the pressing of the start button in the startpage, initializes responses of user and redirects user to first question of survey.
+    """
+
+    # initialize flask sesion saved cookie for user responses
+    session["responses"] = []
+
+    # redirect to first question
+    return redirect('/questions/0')
+
 
 
 @app.route('/questions/<int:question_id>')
@@ -45,10 +62,11 @@ def show_desired_question(question_id):
     TODO: Step 7: Flash Messages
     Using flash, if the user does try to tinker with the URL and visit questions out of order, flash a message telling them they're trying to access an invalid question as part of your redirect.
 
-    TODO: Step 8: didnt know about session so ill leave it here for now.
     """
+    RESPONSES = session["responses"] #STEP 8 Refactoring for flask session inclusion
 
     # Once they've answered all of the questions, trying to access any of the question pages should redirect them to the thank you page.
+
     if len(RESPONSES) == len(selected_survey.questions):
 
         flash(f"You have completed the survey. You can close this page now. You will never be able to restart the survey onece finished, as stated beforehand there is no going back.")
@@ -85,8 +103,11 @@ def handle_question_answer():
     # print(request.args["answer"])
 
     answer = request.args.get("answer")
+    
+    RESPONSES = session["responses"] #STEP 8 Refactoring for flask session inclusion
     # append answer to the fake DB
     RESPONSES.append(answer)
+    session["responses"] = RESPONSES
     
     # move to the next question, assuming the user desnt stop and resubmit their question somehow X.X
     next_question_index = len(RESPONSES)
@@ -104,4 +125,6 @@ def handle_question_answer():
 @app.route('/Thank_you')
 def redirect_to_thank_you_page():
     """render the thank you page"""
+
+    RESPONSES = session["responses"] #STEP 8 Refactoring for flask session inclusion
     return render_template("thank_you.html", responses=RESPONSES)
